339a64c357234e9b95d655db40b78bf4e9ac1df5
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-03-23 19:50:50 +0100

    remplissage des modeles User et Sauce

--- app/Models/Sauce.php
+++ app/Models/Sauce.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sauce extends Model
 {
-    //
+    protected $table = 'sauces'; // Nom de la table
+
+    protected $fillable = [
+        'userId',
+        'name',
+        'manufacturer',
+        'description',
+        'mainPepper',
+        'imageUrl',
+        'heat',
+        'likes',
+        'dislikes',
+        'usersLiked',
+        'usersDisliked',
+    ];
+
+    /**
+     * Casts pour transformer les champs JSON en tableaux PHP.
+     */
+    protected $casts = [
+        'usersLiked' => 'array',
+        'usersDisliked' => 'array',
+    ];
+
+    public function User()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function Sauce()
+    {
+        return $this->hasMany('App\Models\Sauce');
+    }
 }
