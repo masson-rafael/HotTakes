5e398323348d117bc7a302d71faef3cfc78138ea
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-03-23 19:56:22 +0100

    creation des routes pour le controlleur sauce

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SauceController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources(['sauces' => SauceController::class,]);
